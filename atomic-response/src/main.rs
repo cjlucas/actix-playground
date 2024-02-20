e52c7ee94fafbebb610eb9b4e0b3da58e8e2c921
Chris Lucas <chris@cjl.io>
2024-02-20 10:08:58 -0500

    rustfmt

--- atomic-response/src/main.rs
+++ atomic-response/src/main.rs
@@ -16,27 +16,29 @@ struct Ping;
 
 impl Handler<Ping> for A {
     type Result = ();
-    
+
     fn handle(&mut self, _: Ping, _ctx: &mut Self::Context) {
-	println!("PONG");
+        println!("PONG");
     }
 }
 
-
 impl Handler<Stop> for A {
     type Result = AtomicResponse<Self, ()>;
-    
+
     fn handle(&mut self, _: Stop, _ctx: &mut Self::Context) -> Self::Result {
-        AtomicResponse::new(Box::pin(async move {
-	    tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
-	}.into_actor(self)))
+        AtomicResponse::new(Box::pin(
+            async move {
+                tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
+            }
+            .into_actor(self),
+        ))
     }
 }
 
 #[actix::main]
 async fn main() {
     println!("here1");
-    let addr = A{}.start();
+    let addr = A {}.start();
     println!("here2");
     let _ = addr.send(Stop).await;
     println!("here3");
--- ctx-wait/src/main.rs
+++ ctx-wait/src/main.rs
@@ -16,30 +16,32 @@ struct Ping;
 
 impl Handler<Ping> for A {
     type Result = ();
-    
+
     fn handle(&mut self, _: Ping, _ctx: &mut Self::Context) {
-	println!("PONG");
+        println!("PONG");
     }
 }
 
-
 impl Handler<Stop> for A {
     type Result = ();
-    
+
     fn handle(&mut self, _: Stop, ctx: &mut Self::Context) {
-	println!("in stop handle");
-	ctx.wait(async move {
-	    println!("before sleep");
-	    tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
-	    println!("after sleep");
-	}.into_actor(self))
+        println!("in stop handle");
+        ctx.wait(
+            async move {
+                println!("before sleep");
+                tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
+                println!("after sleep");
+            }
+            .into_actor(self),
+        )
     }
 }
 
 #[actix::main]
 async fn main() {
     println!("here1");
-    let addr = A{}.start();
+    let addr = A {}.start();
     println!("here2");
     let _ = addr.send(Stop).await;
     println!("here3");
